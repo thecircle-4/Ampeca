b6cd75b3343cefdc9affd7b005300b9d51b71474
firasjedidi <firasjedidi.com>
2022-02-09 10:45:51 +0100

    routes for post song/get song/update playlist/delete song from playlist done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var routes = require('./routes/routes')
 var bodyParser = require('body-parser')
 var app=express();
+// testing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api",routes)
