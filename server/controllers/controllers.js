937a75c24dc823894528d0874fb9bd1eb11cc524
Mohamed-Haouach <haouachmohamed84@gmail.com>
2022-02-09 10:45:17 +0100

    authentication with jwt done

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -101,7 +101,33 @@ const login =
       }
     );
   });
-
+const getuser =
+  (signupValidation,
+  (req, res, next) => {
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith("Bearer") ||
+      !req.headers.authorization.split(" ")[1]
+    ) {
+      return res.status(422).json({
+        message: "Please provide the token",
+      });
+    }
+    const theToken = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(theToken, "the-super-strong-secrect");
+    db.db.query(
+      "SELECT * FROM user where id=?",
+      decoded.id,
+      function (error, results, fields) {
+        if (error) throw error;
+        return res.send({
+          error: false,
+          data: results[0],
+          message: "Fetch Successfully.",
+        });
+      }
+    );
+  });
 
 
 const remove = (req, res) => {
